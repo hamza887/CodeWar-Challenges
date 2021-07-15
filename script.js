c27ba319bb05e18ed53d867f41d1af8b7293cec7
hamza887 <39979367+hamza887@users.noreply.github.com>
2021-07-15 06:38:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function squareDigits(num){
     return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
     
   }
+
+////WHO LIKES IT /////////
+
+function likes(names) {
+  if(names.length === 0) return "no one likes this";
+  if(names.length === 1) return names[0] + " likes this";
+  if(names.length === 2) return names[0] + " and " + names[1] + " like this";
+  if(names.length === 3) return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+  return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+}
