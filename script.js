34d09630225cbf3bac46056d623a0a34e2b409a0
Hamza Zaka <39979367+hamza887@users.noreply.github.com>
2021-08-26 00:53:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -136,3 +136,15 @@ console.log(frontThree("Python"))
 
 console.log(frontThree("Cucumber"))
 //  ➞ "CucCucCuc"
+
+
+/// covid cases 
+
+ function endCorona(recovers, newCases, activeCases) {
+	return Math.ceil(activeCases/(recovers-newCases))
+}
+endCorona(4000, 2000, 77000) ➞ 39
+
+endCorona(3000, 2000, 50699) ➞ 51
+
+endCorona(30000, 25000, 390205) ➞ 79
