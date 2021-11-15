bea8a28df72d0fc26ad66f091a480a1896ceb792
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-15 14:06:05 -0500

    adding filter

---
+++ Shuffle the String/script.js
@@ -0,0 +1,15 @@
+// EDIBAT
+
+// Write a function that takes all even-indexed characters and odd-indexed characters from a string and concatenates them together
+
+function indexShuffle(str) {
+	const splitLetter= str.split('')
+   const even= splitLetter.filter((a,b)=>b%2==0).join('')
+   const odd=splitLetter.filter((a,b)=>b%2).join('')
+   const h=splitLetter.filter((a,b)=>console.log(b))
+
+   return even+odd
+
+}
+
+console.log(indexShuffle("abcdefg"))
