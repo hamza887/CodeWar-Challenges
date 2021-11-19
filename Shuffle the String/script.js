8d936540c5b474e9997ff7706f557d2d31868789
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-18 20:41:10 -0500

    working on algo

--- Shuffle the String/script.js
+++ Shuffle the String/script.js
@@ -7,12 +7,7 @@ function indexShuffle(str) {
    const even= splitLetter.filter((a,b)=>b%2==0).join('')
    const odd=splitLetter.filter((a,b)=>b%2).join('')
    const h=splitLetter.filter((a,b)=>console.log(b))
-
    return even+odd
-
 }
-
-
-
 
 console.log(indexShuffle("abcdefg"))
