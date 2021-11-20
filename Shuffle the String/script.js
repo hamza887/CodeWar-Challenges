b234063328d4e2a3a95bdb4f20ec7178411fe374
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2021-11-19 23:23:46 -0500

    different updates

--- Shuffle the String/script.js
+++ Shuffle the String/script.js
@@ -4,6 +4,8 @@
 
 function indexShuffle(str) {
 	const splitLetter= str.split('')
+
+   
    const even= splitLetter.filter((a,b)=>b%2==0).join('')
    const odd=splitLetter.filter((a,b)=>b%2).join('')
    const h=splitLetter.filter((a,b)=>console.log(b))
